8a48a66bdc31b3e8663503562e3726d8ae3543e7
HarjeetBaghel24 <harjeetbaghel264@gmail.com>
2026-02-20 15:44:40 +0530

    push this code

---
+++ classes-objects/src/com/gla/Classes/level1/ItemValue.java
@@ -0,0 +1,28 @@
+package com.gla.Classes.level1;
+
+import java.util.Scanner;
+class ItemValue{
+    int itemCode;
+    String itemName;
+    double price;
+
+    void display(int quantity) {
+        System.out.println("Item Code : " + itemCode);
+        System.out.println("Item Name : " + itemName);
+        System.out.println("Price     : " + price);
+        System.out.println("Quantity  : " + quantity);
+        System.out.println("Total Cost: " + (price * quantity));
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Item i = new Item();
+
+        i.itemCode = 201;
+        i.itemName = "Pen";
+        i.price = 10;
+
+        i.display(5);
+    }
+}
